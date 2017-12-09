2831c23c5ac9e1610b184c36f59ee8e4e219772b
Simon Rost <rost.simon@frontfight.net>
2017-12-09 15:36:41 +0100

    initial commit

---
+++ src/main/java/de/robotricker/electricwires/ElectricWires.java
@@ -0,0 +1,12 @@
+package de.robotricker.electricwires;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ElectricWires extends JavaPlugin{
+	
+	@Override
+	public void onEnable() {
+		
+	}
+
+}
