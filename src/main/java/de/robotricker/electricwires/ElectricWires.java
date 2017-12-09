a5481ead4814cc21ad66272aa8a2b00cee129c51
Simon Rost <rost.simon@frontfight.net>
2017-12-09 15:46:47 +0100

    added sentry

--- src/main/java/de/robotricker/electricwires/ElectricWires.java
+++ src/main/java/de/robotricker/electricwires/ElectricWires.java
@@ -1,12 +1,36 @@
 package de.robotricker.electricwires;
 
+import java.lang.Thread.UncaughtExceptionHandler;
+
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class ElectricWires extends JavaPlugin{
+import io.sentry.Sentry;
+import io.sentry.event.UserBuilder;
+
+public class ElectricWires extends JavaPlugin {
 	
+	public static ElectricWires instance;
+
 	@Override
 	public void onEnable() {
+		instance = this;
 		
+		initSentryOnCurrentThread();
 	}
 
+	public static void initSentryOnCurrentThread() {
+		Sentry.init("https://6e99a13e8f654066b8cd00927079db36:3d1a8cf711444b80bda2a6dae0b2ac9e@sentry.io/256964");
+		Sentry.getContext().setUser(new UserBuilder().setUsername("RoboTricker").build());
+		Sentry.getContext().addTag("thread", Thread.currentThread().getName());
+		Sentry.getContext().addTag("version", ElectricWires.instance.getDescription().getVersion());
+
+		Thread.setDefaultUncaughtExceptionHandler(new UncaughtExceptionHandler() {
+			
+			@Override
+			public void uncaughtException(Thread t, Throwable e) {
+				Sentry.capture(e);
+			}
+		});
+	}
+	
 }
